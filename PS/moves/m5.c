d0e133bf7a7f19530a8f3276ff3c1172617f5637
Xabier Matute Rusinol <xmatute-@c3r5s1.42urduliz.com>
2022-11-30 10:16:41 +0100

    testeado y corregido el m5

--- PS/moves/m5.c
+++ PS/moves/m5.c
@@ -6,38 +6,43 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 17:16:29 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/11/29 22:37:28 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/11/30 10:12:29 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
+
+void	pushsmaller5(t_nlist	**a, t_nlist	**b, size_t	n)
+{
+	if (!(ft_nlstsmaller(*a, (*a)->num) < n))
+	{
+		if (ft_nlstsmaller(*a, (*a)->next->num) < n)
+			ra(a);
+		else if (ft_nlstsmaller(*a, ft_nlstlast(*a)->num) < n)
+			rra(a);
+		else if (ft_nlstsmaller(*a, (*a)->next->next->num) < n)
+		{
+			ra(a);
+			ra(a);
+		}
+		else if (ft_nlstsmaller(*a, ft_nlstpenlast(*a)->num) < n)
+		{
+			rra(a);
+			rra(a);
+		}
+	}
+		pb(a, b);
+}
+
 void case5(t_nlist	**a, t_nlist	**b)
 {
 	check(a, b);
-	while (ft_nlstsize(*a) > 3)
-	{
-		if (ft_nlstsmaller(*a, (*a)->num) <= 2)
-			pb(a, b);
-		else if (ft_nlstsmaller(*a, (*a)->next->num) <= 2)
-			ra(a);
-		else if (ft_nlstsmaller(*a, ft_nlstlast(*a)->num) <= 2)
-			rra(a);
-		else if (ft_nlstsmaller(*a, (*a)->next->next->num) <= 2)
-		{
-			ra(a);
-			ra(a);
-		}
-		else if (ft_nlstsmaller(*a, ft_nlstpenlast(*a)->num) <= 2)
-		{
-			rra(a);
-			rra(a);
-		}
-		pb(a, b);
-	}
+	pushsmaller5(a, b, 2);
+	pushsmaller5(a, b, 1);
 	o3a(a, b);
+	swap_in(a, b);
 	pa(a, b);
 	pa(a, b);
 	check(a, b);
-	case5(a, b);
 }
